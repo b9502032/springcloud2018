c55aabae9b541d92eb0a196ef11982e9c8a3cd71
b9502032 <b9502032@gmail.com>
2024-01-06 11:54:49 +0800

    achieve base rest request to provider

---
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/DeptConmsumer80_App.java
@@ -0,0 +1,11 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeptConmsumer80_App {
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConmsumer80_App.class,args);
+    }
+}
---
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/controller/DeptController_Consumer.java
@@ -0,0 +1,49 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@RestController
+public class DeptController_Consumer {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    private final static String REST_URL_PREFIX = "http://localhost:8001";
+
+    @RequestMapping(value = "/consumer/dept/add")
+    public boolean add(Dept dept) {
+        return restTemplate.postForObject(REST_URL_PREFIX + "/dept/add", dept, Boolean.class);
+    }
+
+
+    @RequestMapping(value = "/consumer/dept/get")
+    public Dept get(@PathVariable("id") Long id) {
+        return restTemplate.postForObject(REST_URL_PREFIX + "/dept/get", id, Dept.class);
+    }
+
+
+    @RequestMapping(value = "/consumer/dept/list")
+    public List<Dept> list() {
+
+        ResponseEntity<List<Dept>> response = restTemplate.exchange(
+                REST_URL_PREFIX + "/dept/list",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<List<Dept>>() {});
+        return response.getBody();
+
+//        return restTemplate.postForObject(REST_URL_PREFIX + "/dept/list", List.class);
+    }
+
+
+}
