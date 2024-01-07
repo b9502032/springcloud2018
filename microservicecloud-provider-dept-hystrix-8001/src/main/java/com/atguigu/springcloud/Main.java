1f1a08b8e918811eeb551700fbd3900d45364c94
b9502032 <b9502032@gmail.com>
2024-01-07 15:44:02 +0800

    wip01

--- microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/Main.java
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/Main.java
@@ -2,14 +2,12 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableDiscoveryClient //服务发现
-public class DeptProvider8003_App {
+public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(DeptProvider8003_App.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
