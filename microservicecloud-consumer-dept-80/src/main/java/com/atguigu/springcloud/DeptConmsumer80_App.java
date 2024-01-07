196ef1c225291cbee3b799dfd04a0273fa7eb3b2
b9502032 <b9502032@gmail.com>
2024-01-07 09:48:23 +0800

    zi ding yi lun xun ce lue

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/DeptConmsumer80_App.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/DeptConmsumer80_App.java
@@ -1,11 +1,14 @@
 package com.atguigu.springcloud;
 
+import com.atguigu.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "MICROSERVICECLOUD-DEPT", configuration= MySelfRule.class)
 public class DeptConmsumer80_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptConmsumer80_App.class,args);
